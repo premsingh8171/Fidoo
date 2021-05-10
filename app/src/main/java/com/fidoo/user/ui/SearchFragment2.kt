5f3982e1bed71b292b6525ebc85ef8d274d7791a
prem <premsingh8171@gmail.com>
2021-05-10 15:38:54 +0530

    worked on searchFragment and sendPackages screen change ui  ofpayment mode

--- app/src/main/java/com/fidoo/user/ui/SearchFragment2.kt
+++ app/src/main/java/com/fidoo/user/ui/SearchFragment2.kt
@@ -1,29 +1,633 @@
 package com.fidoo.user.ui
 
+import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.fidoo.user.CartActivity
 import com.fidoo.user.R
+import com.fidoo.user.data.model.AddCartInputModel
+import com.fidoo.user.data.model.CustomCheckBoxModel
+import com.fidoo.user.data.model.SearchModel
+import com.fidoo.user.data.model.TempProductListModel
+import com.fidoo.user.data.session.SessionTwiclo
+import com.fidoo.user.interfaces.AdapterAddRemoveClick
+import com.fidoo.user.interfaces.AdapterCartAddRemoveClick
+import com.fidoo.user.interfaces.AdapterClick
+import com.fidoo.user.search.adapter.ParentStoreListAdapter
+import com.fidoo.user.search.model.Store
+import com.fidoo.user.utils.CommonUtils
+import com.fidoo.user.viewmodels.SearchFragmentViewModel
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_search_item.*
+import kotlinx.android.synthetic.main.fragment_search2.*
+import kotlinx.android.synthetic.main.fragment_search2.view.*
 
 
-class SearchFragment2 : Fragment() {
-	// TODO: Rename and change types of parameters
-	private var param1: String? = null
-	private var param2: String? = null
+class SearchFragment2 : Fragment() , AdapterClick,
+		AdapterAddRemoveClick,
+		AdapterCartAddRemoveClick {
+	private lateinit var parentStoreListAdapter: ParentStoreListAdapter
+	val storeList: ArrayList<Store> = ArrayList()
+	var viewmodel: SearchFragmentViewModel? = null
+	lateinit var mProductStoreList: ArrayList<SearchModel.Store>
+	lateinit var mProductsList: ArrayList<SearchModel.ProductList>
+	var storeID: String = ""
+	var tempType: String? = ""
+	var search_value: String? = ""
+	var customIdsList: ArrayList<String>? = null
+	var customIdsListTemp: ArrayList<CustomCheckBoxModel>? = null
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-
-	}
+	var tempOfferPrice: String? = ""
+	var tempPrice: Double? = 0.0
+	var tempCount: String? = ""
+	var tempProductId: String? = ""
+	var mCustomizeCount: Int? = 0
+	lateinit var mView : View
+	private var _progressDlg: ProgressDialog? = null
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_search2, container, false)
+		mView= inflater.inflate(R.layout.fragment_search2, container, false)
+
+		viewmodel = ViewModelProviders.of(requireActivity()).get(SearchFragmentViewModel::class.java)
+		mProductStoreList= ArrayList()
+		mProductsList= ArrayList()
+		customIdsList= ArrayList()
+		customIdsListTemp= ArrayList()
+
+
+
+		mView.searchEdt_new_fgmt?.addTextChangedListener(object : TextWatcher {
+
+			override fun afterTextChanged(s: Editable) {}
+
+			override fun beforeTextChanged(
+					s: CharSequence, start: Int,
+					count: Int, after: Int
+			) {}
+
+			override fun onTextChanged(
+					s: CharSequence, start: Int,
+					before: Int, count: Int
+			) {
+				search_value=s.toString()
+				if (searchEdt_new?.text.toString().length >= 2) {
+					if (SessionTwiclo(requireContext()).isLoggedIn) {
+						showIOSProgress()
+						viewmodel?.getSearchApi(SessionTwiclo(requireContext()).loggedInUserDetail.accountId,
+								SessionTwiclo(requireContext()).loggedInUserDetail.accessToken, search_value!!)
+
+					}
+				} else {
+					search_value=""
+					mProductStoreList.clear()
+					mProductsList.clear()
+					parentStoreListAdapter.notifyDataSetChanged()
+					total_itemTxt.text = "Items ("+mProductsList.size.toString()+")"
+
+				}
+
+			}
+		})
+
+
+		//Here we have called Api of getGroceryProducts
+		viewmodel?.getSearchApi(
+				SessionTwiclo(context).loggedInUserDetail.accountId,
+				SessionTwiclo(context).loggedInUserDetail.accessToken,
+				""
+		)
+
+		viewmodel?.getCartCountApi(
+				SessionTwiclo(context).loggedInUserDetail.accountId,
+				SessionTwiclo(context).loggedInUserDetail.accessToken
+		)
+
+
+		getResponce()
+
+
+		mView.new_viewcartfromSearch_fgmt.setOnClickListener {
+			if (SessionTwiclo(context).isLoggedIn) {
+				startActivity( Intent(context, CartActivity::class.java).putExtra(
+						"store_id", SessionTwiclo(
+						context
+				).storeId
+				)
+				)
+			} else {
+				showLoginDialog("Please login to proceed")
+
+			}
+		}
+
+		return mView
 	}
+
+	private fun getResponce() {
+
+		//Here we observe searchApi responce
+		viewmodel?.searchResponse?.observe(requireActivity(),{searchResponce->
+			mProductStoreList.clear()
+			mProductsList.clear()
+			dismissIOSProgress()
+			if (!searchResponce.error!!){
+
+
+				val storeList: ArrayList<SearchModel.Store> = ArrayList()
+				val productList: ArrayList<SearchModel.ProductList> = ArrayList()
+
+				val mModelData: SearchModel = searchResponce
+				for (i in 0 until mModelData.store.size) {
+					val storeModel = mModelData.store[i]
+					for (j in 0 until mModelData.store[i].list.size) {
+						val productData = mModelData.store[i].list[j]
+						productList.add(productData)
+					}
+
+					storeList.add(storeModel)
+				}
+				mProductsList=productList
+				mProductStoreList=storeList
+
+				total_itemTxt_fgmt.text = "Items ("+mProductsList.size.toString()+")"
+				parentStoreListAdapter = ParentStoreListAdapter(
+						requireActivity(),mProductStoreList,this,storeID,this,this
+				)
+				mView.search_rv_fgmt?.adapter=parentStoreListAdapter
+
+
+
+			}
+
+		})
+
+		//cartcount responce
+		viewmodel?.cartCountResponse?.observe(requireActivity(),{cartcount->
+			CommonUtils.dismissIOSProgress()
+			dismissIOSProgress()
+			Log.d("cartCountResponse___",cartcount.toString())
+			var count = cartcount.count
+			var price = cartcount.price
+			storeID = cartcount.store_id
+			if (!cartcount.error){
+				if (count!="0"){
+					mView.new_itemQuantity_textsearch_fgmt.text=count
+					mView.new_totalprice_txtsearch_fgmt.text= "₹"+price
+					mView.new_cartitemView_LLsearch_fgmt.visibility= View.VISIBLE
+					SessionTwiclo(context).storeId=cartcount.store_id
+					// setStoreId
+				}else{
+					mView.new_cartitemView_LLsearch_fgmt.visibility= View.GONE
+				}
+			}
+
+		})
+
+		viewmodel?.addRemoveCartResponse?.observe(requireActivity(), { user ->
+			dismissIOSProgress()
+
+			Log.e("cart_response", Gson().toJson(user))
+			if (SessionTwiclo(context).isLoggedIn) {
+				viewmodel?.getSearchApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken,
+						search_value!!
+				)
+				viewmodel?.getCartCountApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken
+				)
+			} else {
+				viewmodel?.getSearchApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken,
+						search_value!!
+				)
+				viewmodel?.getCartCountApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken
+				)
+			}
+
+			//   Toast.makeText(this, "welcocsd", Toast.LENGTH_LONG).show()
+		})
+
+		//add to cart
+		viewmodel?.addToCartResponse?.observe(requireActivity(), Observer { user ->
+			dismissIOSProgress()
+			Log.e("stores response", Gson().toJson(user))
+			val mModelData: com.fidoo.user.data.model.AddToCartModel = user
+			if (tempType.equals("custom")) {
+			} else {
+				MainActivity.tempProductList!!.clear()
+				MainActivity.addCartTempList!!.clear()
+
+			}
+			viewmodel?.getCartCountApi(
+					SessionTwiclo(context).loggedInUserDetail.accountId,
+					SessionTwiclo(context).loggedInUserDetail.accessToken
+			)
+			//showToast(mModelData.message)
+		//	if (isNetworkConnected) {
+				//showIOSProgress()
+				if (SessionTwiclo(context).isLoggedIn) {
+					viewmodel?.getSearchApi(
+							SessionTwiclo(context).loggedInUserDetail.accountId,
+							SessionTwiclo(context).loggedInUserDetail.accessToken,
+							search_value!!
+					)
+				} else {
+					viewmodel?.getSearchApi(
+							SessionTwiclo(context).loggedInUserDetail.accountId,
+							SessionTwiclo(context).loggedInUserDetail.accessToken,
+							search_value!!
+					)
+				}
+
+			//} else {
+			//	showInternetToast()
+			//}
+			//   Toast.makeText(this, "welcocsd", Toast.LENGTH_LONG).show()
+		})
+
+		//clear cart all item
+		viewmodel?.clearCartResponse?.observe(requireActivity(), Observer { user ->
+			// dismissIOSProgress()
+
+			Log.e("stores_response", Gson().toJson(user))
+			if (tempType.equals("custom")) {
+
+				viewmodel!!.addToCartApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken,
+						MainActivity.addCartTempList!!,
+						""
+				)
+				viewmodel?.getCartCountApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken
+				)
+			} else {
+				viewmodel!!.addToCartApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken,
+						MainActivity.addCartTempList!!,
+						""
+
+				)
+				viewmodel?.getCartCountApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken
+				)
+			}
+			//   Toast.makeText(this, "welcocsd", Toast.LENGTH_LONG).show()
+		})
+
+		//error
+		viewmodel?.failureResponse?.observe(requireActivity(), Observer { user ->
+			Log.e("cart_response", Gson().toJson(user))
+		})
+
+	}
+
+	private fun showLoginDialog(message: String){
+		val builder = androidx.appcompat.app.AlertDialog.Builder(requireContext())
+		builder.setTitle("Alert")
+		builder.setMessage(message)
+		builder.setPositiveButton("Login") { dialogInterface, which ->
+//            startActivity(
+//                Intent(activity, LoginActivity::class.java)
+//            )
+
+		}
+
+		//performing negative action
+		builder.setNegativeButton("Cancel") { _, _ ->
+
+		}
+		// Create the AlertDialog
+		val alertDialog: androidx.appcompat.app.AlertDialog = builder.create()
+		alertDialog.setCancelable(true)
+		alertDialog.show()
+	}
+
+	private fun clearCartPopup() {
+		val builder = AlertDialog.Builder(requireContext())
+		builder.setTitle("Replace cart item!")
+		builder.setMessage("Do you want to discard the previous selection?")
+		builder.setIcon(android.R.drawable.ic_dialog_alert)
+		builder.setPositiveButton("Yes") { _, _ ->
+			viewmodel?.clearCartApi(
+					SessionTwiclo(context).loggedInUserDetail.accountId,
+					SessionTwiclo(context).loggedInUserDetail.accessToken
+			)
+		}
+
+		//performing negative action
+		builder.setNegativeButton("No") { dialogInterface, which ->
+		}
+		val alertDialog: AlertDialog = builder.create()
+		alertDialog.setCancelable(false)
+		alertDialog.show()
+	}
+
+
+	// this click to use for customize item first time add
+	override fun onItemClick(productId: String?, type: String?, count: String?, offerPrice: String?, customize_count: Int?, productType: String?, cart_id: String?)
+	{
+		/*  if (behavior?.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+              behavior?.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+          } else {
+              behavior?.setState(BottomSheetBehavior.STATE_COLLAPSED);
+
+          }*/
+
+		tempType = type
+		tempCount = count
+		tempProductId = productId
+		mCustomizeCount = customize_count
+		tempOfferPrice = offerPrice
+
+
+		if (type == "custom") {
+			if (mCustomizeCount == 0) {
+				customIdsListTemp?.clear()
+				customIdsList!!.clear()
+				if (productId != null) {
+					viewmodel?.customizeProductApi(
+							SessionTwiclo(context).loggedInUserDetail.accountId,
+							SessionTwiclo(context).loggedInUserDetail.accessToken, productId
+					)
+				}
+			} else {
+				val builder = AlertDialog.Builder(requireContext())
+				builder.setTitle("Your previous customization")
+				builder.setMessage(tempCount)
+				builder.setPositiveButton("I'LL CHOOSE") { dialogInterface, which ->
+
+					customIdsList!!.clear()
+					if (productId != null) {
+						viewmodel?.customizeProductApi(
+								SessionTwiclo(context).loggedInUserDetail.accountId,
+								SessionTwiclo(context).loggedInUserDetail.accessToken, productId
+						)
+					}
+				}
+
+				//performing negative action
+				builder.setNegativeButton("REPEAT") { dialogInterface, which ->
+
+				}
+				// Create the AlertDialog
+				val alertDialog: AlertDialog = builder.create()
+				// Set other dialog properties
+				alertDialog.setCancelable(true)
+				alertDialog.show()
+			}
+
+
+		} else {
+
+			if (SessionTwiclo(context).storeId.equals(storeID) || SessionTwiclo(context).storeId.equals("")
+			) {
+				SessionTwiclo(context).storeId = storeID
+				Log.e("  store id", SessionTwiclo(context).storeId.toString())
+				showIOSProgress()
+
+				viewmodel!!.addToCartApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken,
+						MainActivity.addCartTempList!!,
+						""
+				)
+				SessionTwiclo(context).storeId = storeID
+			} else {
+				clearCartPopup()
+			}
+		}
+	}
+
+	//for plus and minus click
+	override fun onItemAddRemoveClick(productId: String?, count: String?, type: String?, price: String?, storeId: String?, cartId: String?, position: Int)
+	{
+		//showIOSProgress()
+		//SessionTwiclo(this).storeId = intent.getStringExtra("storeId")
+		if (type.equals("add")) {
+
+			if (MainActivity.tempProductList!!.size == 0) {
+				//customAddBtn.text = resources.getString(R.string.ruppee) + tempPrice.toString()
+				val tempProductListModel = TempProductListModel()
+				tempProductListModel.productId = productId
+				tempProductListModel.quantity = count
+				tempProductListModel.price = price
+				MainActivity.tempProductList!!.add(tempProductListModel)
+
+				val addCartInputModel = AddCartInputModel()
+				addCartInputModel.productId = productId
+				addCartInputModel.quantity = count
+				addCartInputModel.message = "add product"
+				addCartInputModel.customizeSubCatId = customIdsList!!
+				addCartInputModel.isCustomize = "0"
+				MainActivity.addCartTempList!!.add(addCartInputModel)
+
+				if (cartId != null) {
+					showIOSProgress()
+					viewmodel!!.addToCartApi(
+							SessionTwiclo(context).loggedInUserDetail.accountId,
+							SessionTwiclo(context).loggedInUserDetail.accessToken,
+							MainActivity.addCartTempList!!,
+							""
+					)
+				}
+				MainActivity.tempProductList!!.clear()
+			} else {
+				var check: String = ""
+				var tempPos: Int = 0
+
+				for (i in 0 until MainActivity.tempProductList!!.size) {
+					Log.e("check1", MainActivity.tempProductList!!.get(i).productId)
+					Log.e("check2", productId!!)
+					if (MainActivity.tempProductList!![i].productId.equals(productId)) {
+						check = "edit"
+						tempPos = i
+						//tempProductList!!.get(i).quantity = count
+						break
+					}
+				}
+				if (check == "edit") {
+
+					MainActivity.tempProductList!![tempPos].quantity = count
+					MainActivity.addCartTempList!![tempPos].quantity = count
+
+				} else {
+
+					val tempProductListModel = TempProductListModel()
+					tempProductListModel.productId = productId
+					tempProductListModel.quantity = count
+					tempProductListModel.price = price
+					MainActivity.tempProductList!!.add(tempProductListModel)
+
+					val addCartInputModel = AddCartInputModel()
+					addCartInputModel.productId = productId
+					addCartInputModel.quantity = count
+					addCartInputModel.message = "add product"
+					addCartInputModel.customizeSubCatId = customIdsList!!
+					addCartInputModel.isCustomize = "0"
+					MainActivity.addCartTempList!!.add(addCartInputModel)
+				}
+				showIOSProgress()
+				viewmodel!!.addToCartApi(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken,
+						MainActivity.addCartTempList!!,
+						""
+				)
+			}
+
+
+		} else {
+			showIOSProgress()
+			viewmodel?.addRemoveCartDetails(
+					SessionTwiclo(context).loggedInUserDetail.accountId,
+					SessionTwiclo(context).loggedInUserDetail.accessToken,
+					productId!!,
+					"remove",
+					"0",
+					"","",
+					customIdsList!!
+			)
+
+		}
+	}
+
+	override fun clearCart() {
+		viewmodel?.clearCartApi(
+				SessionTwiclo(context).loggedInUserDetail.accountId,
+				SessionTwiclo(context).loggedInUserDetail.accessToken
+		)
+	}
+
+	//on add item first time
+	override fun onAddItemClick(productId: String?, items: String?, offerPrice: String?, isCustomize: String?, prodcustCustomizeId: String?, cart_id: String?)
+	{
+		tempOfferPrice = offerPrice
+		//plusMinusPrice = 0.0
+		tempPrice = 0.0
+		tempProductId = productId
+
+		val builder = AlertDialog.Builder(requireContext())
+		builder.setTitle("Your previous customization")
+		builder.setMessage(items)
+		builder.setPositiveButton("I'LL CHOOSE") { _, which ->
+
+
+
+			//tempProductId = productId
+			//showIOSProgress()
+			customIdsList!!.clear()
+			viewmodel?.customizeProductApi(
+					SessionTwiclo(context).loggedInUserDetail.accountId,
+					SessionTwiclo(context).loggedInUserDetail.accessToken, productId!!
+			)
+		}
+
+		//performing negative action
+		builder.setNegativeButton("REPEAT") { _, which ->
+			//Toast.makeText(applicationContext,"clicked No",Toast.LENGTH_LONG).show()
+			//showIOSProgress()
+			viewmodel?.addRemoveCartDetails(
+					SessionTwiclo(context).loggedInUserDetail.accountId,
+					SessionTwiclo(context).loggedInUserDetail.accessToken,
+					productId!!,
+					"add",
+					isCustomize!!,
+					prodcustCustomizeId!!,
+					cart_id!!,
+					customIdsList!!
+			)
+
+
+
+		}
+		// Create the AlertDialog
+		val alertDialog: AlertDialog = builder.create()
+		// Set other dialog properties
+		alertDialog.setCancelable(true)
+		alertDialog.show()
+	}
+
+	//customize item minus click
+	override fun onRemoveItemClick(productId: String?, quantity: String?, isCustomize: String?, prodcustCustomizeId: String?, cart_id: String?) {
+			if (cart_id != null) {
+				showIOSProgress()
+				viewmodel?.addRemoveCartDetails(
+						SessionTwiclo(context).loggedInUserDetail.accountId,
+						SessionTwiclo(context).loggedInUserDetail.accessToken,
+						productId!!,
+						"remove",
+						isCustomize!!,
+						prodcustCustomizeId!!,
+						cart_id,
+						customIdsList!!
+				)
+			}
+	}
+
+	override fun onResume() {
+		super.onResume()
+		//Here we have called Api of getGroceryProducts
+		viewmodel?.getSearchApi(
+				SessionTwiclo(context).loggedInUserDetail.accountId,
+				SessionTwiclo(context).loggedInUserDetail.accessToken,
+				search_value!!
+		)
+		viewmodel?.getCartCountApi(
+				SessionTwiclo(context).loggedInUserDetail.accountId,
+				SessionTwiclo(context).loggedInUserDetail.accessToken
+		)
+
+	}
+
+	fun showIOSProgress() {
+		closeProgress()
+		_progressDlg = ProgressDialog(requireContext(), R.style.TransparentProgressDialog)
+		_progressDlg!!.getWindow()!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+		_progressDlg!!.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+		_progressDlg!!.setCancelable(false)
+		_progressDlg!!.show()
+	}
+
+	fun closeProgress() {
+		if (_progressDlg == null) {
+			return
+		}
+		_progressDlg!!.dismiss()
+		_progressDlg = null
+	}
+
+	fun dismissIOSProgress() {
+		if (_progressDlg == null) {
+			return
+		}
+		_progressDlg!!.dismiss()
+		_progressDlg = null
+	}
+
 
 }
