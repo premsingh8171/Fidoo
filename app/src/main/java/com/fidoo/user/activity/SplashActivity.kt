51d143d8e8a1e372587879319a3773d0294f62ec
premsingh8171 <premsingh8171@gmail.com>
2022-02-02 00:29:08 +0530

    lived version 1.0.42

--- app/src/main/java/com/fidoo/user/activity/SplashActivity.kt
+++ app/src/main/java/com/fidoo/user/activity/SplashActivity.kt
@@ -32,7 +32,7 @@ class SplashActivity : BaseActivity() {
     private var mMixpanel: MixpanelAPI? = null
 
     companion object {
-        var appversion: String? = "1.0.41"
+        var appversion: String? = "1.0.42"
         var mobile_number: String? = ""
         var splashActivity: SplashActivity? = null
     }
@@ -56,7 +56,7 @@ class SplashActivity : BaseActivity() {
 
         window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
         setContentView(R.layout.activity_splash)
-        appversion = "1.0.41"
+        appversion = "1.0.42"
       //  appversion = BuildConfig.VERSION_NAME
 
         window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
