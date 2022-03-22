65983dce9d66ab3dfce858c6faa9b172d327372f
Nishant Rishab <imreprisal@gmail.com>
2022-03-23 01:36:47 +0530

    commit

--- app/src/main/java/com/fidoo/user/dashboard/listener/OnItemClickBottom.kt
+++
@@ -1,7 +0,0 @@
-package com.fidoo.user.dashboard.listener
-
-import com.fidoo.user.addressmodule.model.GetAddressModel
-
-//interface OnItemClickBottom {
-//    fun onClick(addressList: MutableList<GetAddressModel.AddressList>)
-//}
