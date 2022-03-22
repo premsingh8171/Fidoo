a35f5681e299ccf6cdd65f5da3a440fade64f60b
Nishant Rishab <imreprisal@gmail.com>
2022-03-23 01:24:41 +0530

    commit

--- app/src/main/java/com/fidoo/user/addressmodule/adapter/AddressesAdapterBottom.kt
+++ app/src/main/java/com/fidoo/user/addressmodule/adapter/AddressesAdapterBottom.kt
@@ -10,16 +10,11 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.fidoo.user.R
-import com.fidoo.user.addressmodule.activity.NewAddAddressActivity
-import com.fidoo.user.addressmodule.activity.SavedAddressesActivity
 import com.fidoo.user.addressmodule.model.GetAddressModel
 import com.fidoo.user.cartview.activity.CartActivity
 import com.fidoo.user.data.session.SessionTwiclo
-import com.fidoo.user.profile.ui.ProfileFragment
 import com.fidoo.user.sendpackages.activity.SendPackageActivity
-import com.google.gson.Gson
-import kotlinx.android.synthetic.main.saved_address_items.view.*
-
+import kotlinx.android.synthetic.main.bottom_sheet_saved_address_items.view.*
 
 class AddressesAdapterBottom(
     val con: Context,
@@ -29,7 +24,7 @@ class AddressesAdapterBottom(
 
 ) : RecyclerView.Adapter<AddressesAdapterBottom.UserViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int) = UserViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.saved_address_items, parent, false)
+        LayoutInflater.from(parent.context).inflate(R.layout.bottom_sheet_saved_address_items, parent, false)
     )
 
     override fun getItemCount() = addressList.size
@@ -38,51 +33,6 @@ class AddressesAdapterBottom(
         //holder.tv_address_title.text = "House No.: " + addressList.get(position).flatNo + "\nBuilding: " + addressList[position].building + "\nLandmark: " + addressList.get(position).landmark + "\n" + "Location: " + addressList.get(position).location
 
         holder.tv_address_title.text = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
-
-        if (ProfileFragment.addManages.equals("add_manage")) {
-            holder.itemView.deleteAdd.visibility = View.VISIBLE
-            holder.itemView.editAdd.visibility = View.VISIBLE
-            holder.itemView.tv_yourAddresses.visibility = View.GONE
-
-            holder.itemView.setassDefaultTxt.visibility = View.VISIBLE
-            holder.itemView.selectedadd_img.visibility = View.VISIBLE
-
-            holder.itemView.deleteAdd.setOnClickListener {
-                setOnDeteleAddListener.onDelete(
-                    addressList.get(position).id,
-                    addressList.get(position)
-                )
-            }
-
-            if (SessionTwiclo(con).userAddressId.equals(addressList[position].id)) {
-                holder.itemView.setassDefaultTxt.visibility = View.VISIBLE
-                holder.itemView.selectedadd_img.visibility = View.VISIBLE
-                holder.itemView.selectedadd_img.setImageResource(R.drawable.filter_on)
-
-            } else {
-                holder.itemView.setassDefaultTxt.visibility = View.GONE
-                holder.itemView.selectedadd_img.visibility = View.VISIBLE
-            }
-
-        }else {
-            holder.itemView.deleteAdd.visibility = View.GONE
-            holder.itemView.editAdd.visibility = View.GONE
-            holder.itemView.setassDefaultTxt.visibility = View.GONE
-            holder.itemView.setassDefaultTxt.visibility = View.GONE
-            holder.itemView.selectedadd_img.visibility = View.GONE
-
-//            if (position == 0) {
-//                holder.itemView.tv_yourAddresses.visibility = View.VISIBLE
-//            } else {
-//                holder.itemView.tv_yourAddresses.visibility = View.GONE
-//            }
-        }
-
-//        if (position==0){
-//           holder.itemView.tv_yourAddresses.visibility=View.VISIBLE
-//        }else{
-//            holder.itemView.tv_yourAddresses.visibility=View.GONE
-//        }
 
         if (addressList[position].inDeliveryRange == "0"){
             holder.mainLay.alpha = 0.5f
@@ -93,7 +43,7 @@ class AddressesAdapterBottom(
                 if (addressList[position].is_default == "1") {
                     holder.storeName.text = "Home (Default)"
                 } else {
-                    holder.storeName.text = "Home"
+                    holder.storeName?.text = "Home"
                 }
                 holder.addressTypeIcon.setImageResource(R.drawable.ic_home)
             }
@@ -121,14 +71,7 @@ class AddressesAdapterBottom(
 
 
         holder.mainLay.setOnClickListener {
-
-//            val dialog = Dialog(con)
-//            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-//            if (dialog.isShowing){
-//                dialog.dismiss()
-//            }
             Log.e("clicked", "clicked")
-            Toast.makeText(con, "Item is clicked", Toast.LENGTH_SHORT).show()
             if (addressList[position].inDeliveryRange == "0"){
                 Toast.makeText(con, "Store doesn't deliver to this location", Toast.LENGTH_LONG).show()
             }
@@ -141,22 +84,16 @@ class AddressesAdapterBottom(
                     CartActivity.selectedPreAddressName = addressList.get(position).flatNo + "\nBuilding: " + addressList.get(position).building + "\nLandmark: " + addressList.get(position).landmark
                     CartActivity.userLat = addressList[position].latitude
                     CartActivity.userLong = addressList[position].longitude
-                    //StoreItemsActivity.customerLatitude = addressList[position].latitude
-                    //StoreItemsActivity.customerLongitude = addressList[position].longitude
                     when {
                         addressList[position].addressType.equals("1") -> {
-                            /// SessionTwiclo(con).userAddress = "Home"
                             CartActivity.selectedAddressTitle= "Home"
                         }
 
                         addressList[position].addressType.equals("2") -> {
                             CartActivity.selectedAddressTitle= "Office"
-
-                            // SessionTwiclo(con).userAddress = "Office"
                         }
 
                         else -> {
-                            // SessionTwiclo(con).userAddress = "Other"
                             CartActivity.selectedAddressTitle = "Other"
 
                         }
@@ -165,11 +102,6 @@ class AddressesAdapterBottom(
                     SessionTwiclo(con).userLng = addressList[position].longitude
                     SessionTwiclo(con).userAddressId = addressList[position].id
                     SessionTwiclo(con).userAddress = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
-                    // SessionTwiclo(con).addressType= addressList[position].addressType
-
-                    // holder.itemView.setassDefaultTxt.visibility=View.VISIBLE
-                    //  holder.itemView.selectedadd_img.visibility=View.VISIBLE
-                    //   holder.itemView.selectedadd_img.setImageResource(R.drawable.filter_on)
                     val result = Intent()
                     (con as Activity).setResult(101, result)
                     (con as Activity).finish()
@@ -180,22 +112,15 @@ class AddressesAdapterBottom(
                             SessionTwiclo(con).userAddress = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
                             SessionTwiclo(con).addressType= "Home"
                         }
-
                         addressList[position].addressType.equals("2") -> {
                             SessionTwiclo(con).userAddress = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
                             SessionTwiclo(con).addressType = "Office"
-
                         }
-
                         else -> {
                             SessionTwiclo(con).userAddress = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
                             SessionTwiclo(con).addressType = "Other"
-
                         }
                     }
-                    // holder.itemView.setassDefaultTxt.visibility=View.VISIBLE
-                    //  holder.itemView.selectedadd_img.visibility=View.VISIBLE
-                    // holder.itemView.selectedadd_img.setImageResource(R.drawable.filter_on)
 
                     SessionTwiclo(con).userAddress = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
                     SessionTwiclo(con).userAddressId = addressList[position].id
@@ -209,37 +134,6 @@ class AddressesAdapterBottom(
                 else if (stringExtra.equals("bottomSheetAddress")) {
                     setOnDeteleAddListener.onClick(addressList[position])
                 }
-                else if (ProfileFragment.addManages.equals("add_manage")) {
-                    Log.e("clickedaddress", "address")
-                    when {
-                        addressList[position].addressType.equals("1") -> {
-                            SessionTwiclo(con).userAddress = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
-                            SessionTwiclo(con).addressType = "Home"
-                        }
-
-                        addressList[position].addressType.equals("2") -> {
-                            SessionTwiclo(con).userAddress = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
-                            SessionTwiclo(con).addressType = "Office"
-
-                        }
-
-                        else -> {
-                            SessionTwiclo(con).userAddress =addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
-                            SessionTwiclo(con).addressType = "Other"
-
-                        }
-                    }
-
-                    holder.itemView.setassDefaultTxt.visibility=View.VISIBLE
-                    holder.itemView.selectedadd_img.visibility=View.VISIBLE
-                    holder.itemView.selectedadd_img.setImageResource(R.drawable.filter_on)
-
-                    SessionTwiclo(con).userAddress = addressList.get(position).flatNo + ", " + addressList.get(position).landmark + ", " + addressList.get(position).location
-                    SessionTwiclo(con).userAddressId = addressList[position].id
-                    SessionTwiclo(con).userLat = addressList[position].latitude
-                    SessionTwiclo(con).userLng = addressList[position].longitude
-                    (con as Activity).finish()
-                }
 
                 if (stringExtra.equals("from")) {
                     SendPackageActivity.selectedFromAddress= addressList.get(position).flatNo + ", Building: " + addressList.get(position).building + ", Landmark: " + addressList.get(position).landmark+", "+addressList[position].location
@@ -250,7 +144,6 @@ class AddressesAdapterBottom(
                     SendPackageActivity.fromNumber = addressList[position].phone_no
                     (con as Activity).finish()
                 }
-
                 if (stringExtra.equals("to")) {
                     SendPackageActivity.selectedToAddress= addressList.get(position).flatNo + ", Building: " + addressList.get(position).building + ", Landmark: " + addressList.get(position).landmark+", "+addressList[position].location
                     /// SendPackageActivity.selectedToAddress = addressList[position].location
@@ -260,40 +153,15 @@ class AddressesAdapterBottom(
                     SendPackageActivity.toNumber = addressList[position].phone_no
                     (con as Activity).finish()
                 }
-
             }
-
-
         }
-
-        // Log.d("ewwewew",stringExtra.equals("address").toString())
-
-        holder.itemView.editAdd.setOnClickListener {
-            if (addressList[position].phone_no.toString().equals("")||addressList[position].name.toString().equals("")){
-                SavedAddressesActivity.addAddressOrNot ="current_location"
-            }else{
-                SavedAddressesActivity.addAddressOrNot ="new_add"
-            }
-
-            // Log.d("ewwewew",addressList[position].phone_no.toString())
-            // if (stringExtra.equals("address")||stringExtra.equals("from")||stringExtra.equals("to")) {
-            con.startActivity(Intent(con, NewAddAddressActivity::class.java).putExtra("data", Gson().toJson(addressList[position])))
-            //    }
-
-            //   true
-        }
-
-
     }
 
     class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var tv_address_title = view.tv_location
         var storeName = view.tv_address_title
         var addressTypeIcon = view.locationImage
-        // var deleteIcon = view.deleteIcon
-        //var nameText = view.nameText
         var mainLay = view.mainLay
-        //var editIcon = view.editIcon
     }
 
     interface SetOnDeteleAddListener{
