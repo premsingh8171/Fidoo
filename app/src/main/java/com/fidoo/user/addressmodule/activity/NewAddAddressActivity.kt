c4cb56f8949eb8b0a956859aad56e94893c653b0
Nishant Rishab <imreprisal@gmail.com>
2022-03-25 00:31:30 +0530

    c

--- app/src/main/java/com/fidoo/user/addressmodule/activity/NewAddAddressActivity.kt
+++ app/src/main/java/com/fidoo/user/addressmodule/activity/NewAddAddressActivity.kt
@@ -244,13 +244,13 @@ open class NewAddAddressActivity : BaseActivity(), OnMapReadyCallback, LocationL
          */
         btn_proceed.setOnClickListener {
             tv_SelectDeliveryAddress.visibility = View.GONE
-            //    add_address_frmLL.visibility = View.GONE
             live_add_1.visibility = View.GONE
             btn_proceed.visibility = View.GONE
             tv_AddAddress.visibility = View.VISIBLE
             add_new_add_ll.visibility = View.VISIBLE
             btn_continue.visibility = View.VISIBLE
-            //add_address_frmLL_One.visibility = View.VISIBLE
+            iv_mapSlider.visibility  = View.GONE
+            iv_emptyMap.visibility = View.VISIBLE
         }
 
         btn_continue.setOnClickListener {
