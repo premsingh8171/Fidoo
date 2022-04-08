51b357329a50dab0d73b3a069db58e187cf04207
shobha242 <shobhatripathi53@gmail.com>
2022-04-08 14:13:38 +0530

    order detail send package page,cart page

--- app/src/main/java/com/fidoo/user/ordermodule/ui/OrderDetailsSendPackageActivity.kt
+++ app/src/main/java/com/fidoo/user/ordermodule/ui/OrderDetailsSendPackageActivity.kt
@@ -90,7 +90,7 @@ class OrderDetailsSendPackageActivity : BaseActivity() {
                         deliveredAtTxt.text = ""
                         delivery_boyTxt.text = ""
                     }
-                    if (user.order_status.equals("2")) {
+                    if (user.order_status.equals("2") && user.order_status.equals("17")) {
                         orderstatus_tv.text = "Cancelled"
                         orderStatusLabel.text = ""
                         deliveredAtTxt.text = ""
@@ -102,7 +102,7 @@ class OrderDetailsSendPackageActivity : BaseActivity() {
                         deliveredAtTxt.text = user.deleivered_at
                         delivery_boyTxt.text = "Order Delivered by " + user.delivery_boy_name
                     }
-                    if (user.order_status.equals("9")) {
+                    if (user.order_status.equals("9") && user.order_status.equals("16")) {
                         orderstatus_tv.text = "In Progress"
                         orderStatusLabel.text = ""
                         deliveredAtTxt.text = ""
