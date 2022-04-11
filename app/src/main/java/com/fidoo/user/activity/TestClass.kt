4efb1ff32c67070dc0f865d55102f92679dea34b
Nishant Rishab <nishant@Fidoo.in>
2022-04-11 03:38:07 -0700

    first

---
+++ app/src/main/java/com/fidoo/user/activity/TestClass.kt
@@ -0,0 +1,20 @@
+package com.fidoo.user.activity
+
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import com.fidoo.user.R
+import com.fidoo.user.utils.BaseActivity
+
+class TestClass : BaseActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard_fiddopay)
+        val window: Window = this.getWindow()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
+    }
+}
