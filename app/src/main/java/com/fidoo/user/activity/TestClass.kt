bf40f51f32b99f24def83a0d66684d2f241f03e9
Nishant Rishab <nishant@Fidoo.in>
2022-04-12 00:38:44 -0700

    second

--- app/src/main/java/com/fidoo/user/activity/TestClass.kt
+++ app/src/main/java/com/fidoo/user/activity/TestClass.kt
@@ -1,6 +1,5 @@
 package com.fidoo.user.activity
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
 import android.widget.*
@@ -8,19 +7,16 @@ import androidx.core.content.ContextCompat
 import com.fidoo.user.R
 import com.fidoo.user.utils.BaseActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import kotlinx.android.synthetic.main.aaaaaaaaaaaaaaaaaaaaaaaaaa.*
+import kotlinx.android.synthetic.main.dashboard_fiddopay.*
 
-
-@Suppress("DEPRECATION")
 class TestClass : BaseActivity(){
     lateinit var behavior: BottomSheetBehavior<LinearLayout>
-    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.statusBarColor = ContextCompat.getColor(this, R.color.colorPrimary)
-        setContentView(R.layout.aaaaaaaaaaaaaaaaaaaaaaaaaa)
+        setContentView(R.layout.dashboard_fiddopay)
         behavior = BottomSheetBehavior.from(bottomSheetBtn)
     }
 }
